0a9d762965a74db0168d854e7ca9297e16146443
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2026-05-18 14:07:24 +0200

    update devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Customise the terminal command prompt
+printf "export PS1='\\[\\e[3;36m\\]\${PWD#/workspaces/} ->\\[\\e[0m\\] '\n" >> $HOME/.bashrc
+export PS1='\[\e[3;36m\]${PWD#/workspaces/} ->\[\e[0m\] '
+
+# Update Nextflow
+nextflow self-update
+nextflow -version
+
+cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt
