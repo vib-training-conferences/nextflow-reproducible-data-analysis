f8e4409d60a977e313d70db1990406aed3a5890f
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2026-05-18 15:26:32 +0200

    fixes to devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,6 +6,3 @@ export PS1='\[\e[3;36m\]${PWD#/workspaces/} ->\[\e[0m\] '
 
 # Update Nextflow
 nextflow self-update
-nextflow -version
-
-cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt
